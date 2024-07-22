d586bed45b3d248694523a9bd1811acadd931b23
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-07-23 02:33:27 +0530

    Signup Component

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from 'react';
+import "./style.css";
 function SignupSigninComponent() {
   return (
-    <div>
-      
+    <div className='signup-wrapper'>
+      SignupSigninComponent
     </div>
   )
 }
