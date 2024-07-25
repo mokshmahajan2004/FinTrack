9bb8e229793a306743459a1428ca5e29d4b1a961
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-07-25 13:32:28 +0530

    Signup Component Form

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./style.css";
-import Input from '../Input';
+import Input from "../Input";
 function SignupSigninComponent() {
-  const [name,setName]=useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   return (
-    <div className='signup-wrapper'>
-     <h2 className='title'>Sign Up on <span style={{color:"var(--theme)"}}>FinTrack.</span></h2>
-    <form>
-      <Input label={"Full Name"} state={name} setState={setName} placeholder={"John Doe"}/>
-    </form>
-    
+    <div className="signup-wrapper">
+      <h2 className="title">
+        Sign Up on <span style={{ color: "var(--theme)" }}>FinTrack.</span>
+      </h2>
+      <form>
+        <Input
+          label={"Full Name"}
+          state={name}
+          setState={setName}
+          placeholder={"John Doe"}
+        />
+        <Input
+          label={"Email"}
+          state={email}
+          setState={setEmail}
+          placeholder={"JohnDoe@gmail.com"}
+        />
+        <Input
+          label={"Password"}
+          state={password}
+          setState={setPassword}
+          placeholder={"Example@123"}
+        />
+        <Input
+          label={"Confirm Password"}
+          state={confirmPassword}
+          setState={setConfirmPassword}
+          placeholder={"Example@123"}
+        />
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignupSigninComponent
+export default SignupSigninComponent;
