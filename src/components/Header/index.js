d785605096c2a4d1c1d320ebc0e35af73fcf329e
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-08-02 19:51:48 +0530

    Logout Functionality Added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import "./style.css";
-
+import { auth } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { toast } from "react-toastify";
 function Header() {
-  function logoutFnc(){
-    alert("Logout!");
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, loading]);
+
+  function logoutFnc() {
+try {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    toast.success("Logged Out Successfully!")
+    navigate("/")
+  }).catch((error) => {
+    // An error happened.
+    toast.error(error.message)
+  });
+} catch (error) {
+  toast.error(error.message)
+  
+}
   }
   return (
-    <div className='navbar'>
-      <p className='logo'>FinTrack</p>
-      <p className='logo link'onClick={logoutFnc}>Logout</p>
+    <div className="navbar">
+      <p className="logo">FinTrack</p>
+      {user && (
+        <p className="logo link" onClick={logoutFnc}>
+          Logout
+        </p>
+      )}
     </div>
-  )
+  );
 }
 
-
-export default Header
+export default Header;
