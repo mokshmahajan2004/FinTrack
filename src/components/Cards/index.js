0cb72505e1312c015700bd4d8bf9733e9bb3972a
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-08-04 03:03:17 +0530

    Dashboard Started

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./style.css"
 import { Card, Row } from 'antd'
+import Button from '../Button'
 function Cards() {
   return (
     <div>
         <Row className='my-row'>
             <Card className='my-card' title="Current Balance">
                 0 Rupees
+                < Button text="Reset Balance"/>
             </Card>
         </Row>
     </div>
