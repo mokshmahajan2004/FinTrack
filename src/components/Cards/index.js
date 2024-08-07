0441ac9f314862d69389cc145e7c8543c3002214
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-08-07 12:44:12 +0530

    Modals Prepared

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -6,18 +6,23 @@ function Cards() {
   return (
     <div>
         <Row className='my-row'>
-            <Card className='my-card' title="Current Balance">
-                0 Rs.
-                < Button text="Reset Balance" blue={true}/>
-            </Card>
-            <Card className='my-card' title="Current Balance">
-                0 Rs.
-                < Button text="Reset Balance" blue={true}/>
-            </Card>
-            <Card className='my-card' title="Current Balance">
-                0 Rs.
-                < Button text="Reset Balance" blue={true}/>
-            </Card>
+        <Card bordered={true} className='my-card'>
+        <h2>Current Balance</h2>
+        <p>₹0</p>
+        <Button text="Reset Balance" blue={true} />
+        </Card>
+
+      <Card bordered={true} className='my-card'>
+        <h2>Total Income</h2>
+        <p>₹0</p>
+        <Button text=" Add Income" blue={true}/>
+      </Card>
+
+      <Card bordered={true} className='my-card'>
+        <h2>Total Expenses</h2>
+        <p>₹0</p>
+        <Button text=" Add Expense" blue={true}/>
+      </Card>
         </Row>
     </div>
   )
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import Header from '../components/Header'
-import Cards from '../components/Cards'
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Cards from "../components/Cards";
 
 function Dashboard() {
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+  const showExpenseModal = () => {
+    setIsExpenseModalVisible(true);
+  };
+
+  const showIncomeModal = () => {
+    setIsIncomeModalVisible(true);
+  };
+
+  const handleExpenseCancel = () => {
+    setIsExpenseModalVisible(false);
+  };
+
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  };
   return (
     <div>
-     <Header />
-     <Cards />
+      <Header />
+      <Cards
+        showExpenseModal={showExpenseModal}
+        showIncomeModal={showIncomeModal}
+        handleExpenseCancel={handleExpenseCancel}
+        handleIncomeCancel={handleIncomeCancel}
+      />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
