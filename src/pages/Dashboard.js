ed85f177a7357477143f02d7c5fd9e69d8b47e5b
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-08-10 16:12:10 +0530

    Adding Transactions in database

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Cards from "../components/Cards";
 import { Modal } from "antd";
 import AddExpenseModal from "../Modals/addExpense";
 import AddIncomeModal from "../Modals/addIncome";
 import moment from "moment";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function Dashboard() {
+  const [user] = useAuthState(auth);
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState([false]);
+  // const transactions = [
+  //   {
+  //     type: "income",
+  //     amount: 1200,
+  //     tag: "salary",
+  //     name: "income1",
+  //     date: "2023-05-23",
+  //   },
+  //   {
+  //     type: "expense",
+  //     amount: 1200,
+  //     tag: "food",
+  //     name: "expense1",
+  //     date: "2023-05-17",
+  //   },
+  // ];
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
   const showExpenseModal = () => {
@@ -24,31 +47,61 @@ function Dashboard() {
   const handleIncomeCancel = () => {
     setIsIncomeModalVisible(false);
   };
-  const onFinish=(values,type)=>{
-    const newTransaction={
-      type:type,
-      date:moment()
+  const onFinish = (values, type) => {
+    const newTransaction = {
+      type: type,
+      date: moment(values.date).format("YYYY-MM-DD"),
+      amount: parseFloat(values.amount),
+      tag: values.tag,
+      name: values.name,
+    };
+    addTransaction(newTransaction);
+  };
+  async function addTransaction(transaction) {
+    try {
+      const docRef = await addDoc(
+        collection(db, `users/${user.uid}/transactions`),
+        transaction
+      );
+      console.log("Document written with ID:", docRef.id);
+      toast.success("Transaction Added");
+    } catch (e) {
+      console.error("Error adding document:", e);
+      toast.error("Couldn't add transaction");
     }
+  }
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+  async function fetchTransactions() {
+    setLoading(true);
+    if (user) {
+      const q = query(collection(db, `users/${user.uid}/transactions`));
+      const querySnapshot = await getDocs(q);
+      let transactionsArray = [];
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        transactionsArray.push(doc.data());
+      });
+      setTransactions(transactionsArray);
+      toast.success("Transactions Fetched!");
+    }
+    setLoading(false);
   }
   return (
     <div>
       <Header />
-      <Cards
-        showExpenseModal={showExpenseModal}
-        showIncomeModal={showIncomeModal}
-        handleExpenseCancel={handleExpenseCancel}
-        handleIncomeCancel={handleIncomeCancel}
-      />
-      <Modal
-        style={{ fontWeight: 600 }}
-        title="Add Income"
-        open={isIncomeModalVisible}
-        onCancel={handleIncomeCancel}
-        footer={null}
-      >
-        Income
-      </Modal>
-      <AddExpenseModal
+      {loading ? (
+        <p>Loading....</p>
+      ) : (
+        <>
+          <Cards
+            showExpenseModal={showExpenseModal}
+            showIncomeModal={showIncomeModal}
+            handleExpenseCancel={handleExpenseCancel}
+            handleIncomeCancel={handleIncomeCancel}
+          />
+          <AddExpenseModal
             isExpenseModalVisible={isExpenseModalVisible}
             handleExpenseCancel={handleExpenseCancel}
             onFinish={onFinish}
@@ -58,6 +111,8 @@ function Dashboard() {
             handleIncomeCancel={handleIncomeCancel}
             onFinish={onFinish}
           />
+        </>
+      )}
     </div>
   );
 }
